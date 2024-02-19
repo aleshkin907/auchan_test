2909142a1c3e448399f6ebaa5d5e3e0f502e5e13
misha <aleshkin.907@gmail.com>
2024-02-19 16:04:07 +0600

    Initial

--- src/main.py
+++ src/main.py
@@ -1,25 +1,28 @@
 import glob
 import re
 
-TEST_FOLDER_MASK = "TEST_Folder_*"
-TEST_FILE_MASK = "TEST_AUCHAN_*"
-RESULT_FOLDER_MASK = "Result"
-RESULT_FILE_MASK = "TEST_AUCHAN_success_"
+TEST_FOLDER_MASK = "TEST_Folder_*"  # Шаблон для поиска папок с тестовыми файлами
+TEST_FILE_MASK = "TEST_AUCHAN_*"     # Шаблон для поиска тестовых файлов внутри папок
+RESULT_FOLDER_MASK = "Result"        # Папка для записи результатов
+RESULT_FILE_MASK = "TEST_AUCHAN_success_"  # Шаблон имени файла для записи результатов
 
 
 class NumberParser:
     @staticmethod
     def parse_ranges(input_str):
         input_str = input_str.replace('"', '').replace("'", "")
+        # Разделение диапазона на начальное и конечное значение
         elements = [x.strip() for x in input_str.split(',')]
         result = []
 
         for element in elements:
             if '-' in element:
                 start, end = element.split('-')
+                # Вычисление ширины числа, учитывая ведущие нули
                 width = len(start) if start[0] == '0' else 0
                 start_num = int(start)
                 end_num = int(end)
+                # Создание списка чисел в диапазоне и добавление их к результату
                 result.extend(str(num).zfill(width) for num in range(start_num, end_num + 1))
             else:
                 result.append(element.strip())
